4a7f4f40798853b2634ad1d9686159b0947f8df3
Guy Burton <guy.burton@isam.com>
2020-04-13 09:51:39 +0100

    Disconnection provides null byte not empty string

--- mido/sockets.py
+++ mido/sockets.py
@@ -104,7 +104,7 @@ class SocketPort(BaseIOPort):
                 byte = self._rfile.read(1)
             except socket.error as err:
                 raise IOError(err.args[1])
-            if byte == '':
+            if byte == b'':
                 # The other end has disconnected.
                 self.close()
                 break
